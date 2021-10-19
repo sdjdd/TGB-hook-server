2826f4fb8b3199bb5bdeff8532f2062177303170
sdjdd <i@sdjdd.com>
2021-10-19 17:10:02 +0800

    refactor: remove unused config

--- config.js
+++ config.js
@@ -2,10 +2,6 @@ module.exports = {
   leanTicket: {
     webhookSecret: process.env.LEANTICKET_WEBHOOK_SECRET,
     host: process.env.LEANTICKET_HOST,
-    appId: process.env.LEANCLOUD_APP_ID,
-    appKey: process.env.LEANCLOUD_APP_KEY,
-    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY,
-    serverURL: process.env.LEANTICKET_SERVER_URL,
   },
   slack: {
     token: process.env.SLACK_TOKEN,
